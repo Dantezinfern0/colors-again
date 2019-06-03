004a642fd9f787610c53e2d6b7050e969369f583
dantezinfern0 <danteharasz@gmail.com>
2019-06-03 13:41:05 -0400

    buttons up, not connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { useState, Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
-  }
+export default function Counter() {
+  return (
+    <h1>ColorChanger</h1>
+    <div>
+    <input type="range" min="0" max="100" name="saturation" value={value} step="1">Saturation</input>
+    <input type="range" min="0" max="359" name="hue" value={value} step="1">Hue</input>
+    <input type="range" min="0" max="100" name="lightness" value={value} step="1">Lightness</input>
+    </div>
+  )
 }
-
-export default App
